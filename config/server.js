7791a3bd4c735cbcedb0d7be6a1b623bf4f2a694
ArcRafa <maldonadoagudelorafael@gmail.com>
2022-07-20 21:47:29 -0500

    server keys heroku

--- config/server.js
+++ config/server.js
@@ -1,7 +1,12 @@
-module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+module.exports = ({env}) => ({
+
+  url: env("MY_HEROKU_URL"),
+
+  proxy: true,
+
   app: {
-    keys: env.array('APP_KEYS'),
+
+    keys: env.array("APP_KEYS", ['keys', 'keys'])
   },
+
 });
